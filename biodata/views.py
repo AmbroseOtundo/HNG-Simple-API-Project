fad23c00d423f94b7efec0838cf318023d6d4a6f
Ambrose Otundo <48138192+AmbroseOtundo@users.noreply.github.com>
2022-10-27 21:02:53 +0300

    overhaul views and use jsonresponse instead

--- biodata/views.py
+++ biodata/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from biodata.models import Profile
-from biodata.serializers import ProfileSerializer
-
+from django.http import JsonResponse
+from rest_framework.response import Response
 # Create your views here.
-class ProfileList(generics.ListAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
+class ProfileList(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+        "slackUsername": "ambroseotundo",
+        "backend": "true",
+        "age": 24,
+        "bio": "I am an avid backend engineer who is on a mission to develop scalable software."
+    }
+        return Response(data)
